4df1c85e03415baf9cdd6b55f59298c3f89e494e
Musah Abdul-Malik <musah.abdulmalikgh@gmail.com>
2021-02-08 21:26:27 +0000

    Add create article view

--- articles/views.py
+++ articles/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
 from .models import Article
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+class  ArticleCreateView(CreateView):
+    model = Article
+    fields = ('title', 'body', 'author', )
+    template_name = "article_new.html"
 
 class ArticleListView(ListView):
     model = Article
     template_name = 'article_list.html'
 
+class  ArticleDetailView(DetailView):
+    model = Article
+    template_name = "article_detail.html"
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ('title', 'body',)
+    template_name = 'article_edit.html'     
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
