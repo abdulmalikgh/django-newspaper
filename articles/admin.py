242b3898cf942a40a6599cb0893801d709bc351c
Musah Abdul-Malik <musah.abdulmalikgh@gmail.com>
2021-02-09 18:01:46 +0000

    Add commit comments

--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Article
+from .models import Comment
+
 # Register your models here.
-admin.site.register(Article)
+class CommentInline(admin.TabularInline):
+    model = Comment
+    default = 0
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline
+    ]
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
